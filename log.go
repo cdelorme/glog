808143b4a1c0aa96ca440a9d19e0151c328f8bc7
Casey DeLorme <cdelorme@gmail.com>
2014-10-30 15:59:02 -0400

    refixed (was an error on my end) and fmt'd

--- log.go
+++ log.go
@@ -7,36 +7,36 @@ const ESCAPE = "\x1b"
 const RESET = 0
 
 const (
-    BLACK Color = iota + 30
-    RED
-    GREEN
-    YELLOW
-    BLUE
-    MAGENTA
-    CYAN
-    WHITE
+	BLACK Color = iota + 30
+	RED
+	GREEN
+	YELLOW
+	BLUE
+	MAGENTA
+	CYAN
+	WHITE
 )
 
 const (
-    DEFAULT LogLevel = iota
-    EMERGENCY
-    ALERT
-    CRITICAL
-    ERROR
-    WARNING
-    NOTICE
-    INFO
-    DEBUG
+	DEFAULT LogLevel = iota
+	EMERGENCY
+	ALERT
+	CRITICAL
+	ERROR
+	WARNING
+	NOTICE
+	INFO
+	DEBUG
 )
 
 var LevelNames = []string{
-    "Debug",
-    "Emergency",
-    "Alert",
-    "Critical",
-    "Error",
-    "Notice",
-    "Warning",
-    "Info",
-    "Debug",
+	"Debug",
+	"Emergency",
+	"Alert",
+	"Critical",
+	"Error",
+	"Notice",
+	"Warning",
+	"Info",
+	"Debug",
 }
--- logger.go
+++ logger.go
@@ -1,102 +1,102 @@
 package log
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type Logger struct {
-    Level   LogLevel
-    Silent  bool
-    NoColor bool
+	Level   LogLevel
+	Silent  bool
+	NoColor bool
 }
 
 func (logger *Logger) log(record *Record, color Color) {
 
-    if !logger.Silent && (logger.Level == DEFAULT || record.Level >= logger.Level) {
-    	parsedMessage := record.Message
+	if !logger.Silent && (logger.Level == DEFAULT || record.Level <= logger.Level) {
+		parsedMessage := record.Message
 
-    	if len(record.Args) > 0 {
-    		parsedMessage = fmt.Sprintf(record.Message, &record.Args)
-    	}
+		if len(record.Args) > 0 {
+			parsedMessage = fmt.Sprintf(record.Message, &record.Args)
+		}
 
-    	parsedMessage = fmt.Sprintf("[%s] %s:%d, %s", record.Date, record.File, record.LineNumber, parsedMessage)
+		parsedMessage = fmt.Sprintf("[%s] %s:%d, %s", record.Date, record.File, record.LineNumber, parsedMessage)
 
-    	if !logger.NoColor {
-    		parsedMessage = fmt.Sprintf("%s[%dm%s%s[%dm", ESCAPE, color, parsedMessage, ESCAPE, RESET)
-    	}
+		if !logger.NoColor {
+			parsedMessage = fmt.Sprintf("%s[%dm%s%s[%dm", ESCAPE, color, parsedMessage, ESCAPE, RESET)
+		}
 
-    	fmt.Println(parsedMessage)
-    }
+		fmt.Println(parsedMessage)
+	}
 }
 
 func (logger *Logger) Emergency(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, EMERGENCY, args...)
+	record := Record{}
+	record.Build(message, 1, EMERGENCY, args...)
 
-    logger.log(&record, RED)
+	logger.log(&record, RED)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Alert(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, ALERT, args...)
+	record := Record{}
+	record.Build(message, 1, ALERT, args...)
 
-    logger.log(&record, RED)
+	logger.log(&record, RED)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Critical(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, CRITICAL, args...)
+	record := Record{}
+	record.Build(message, 1, CRITICAL, args...)
 
-    logger.log(&record, MAGENTA)
+	logger.log(&record, MAGENTA)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Error(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, ERROR, args...)
+	record := Record{}
+	record.Build(message, 1, ERROR, args...)
 
-    logger.log(&record, MAGENTA)
+	logger.log(&record, MAGENTA)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Notice(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, NOTICE, args...)
+	record := Record{}
+	record.Build(message, 1, NOTICE, args...)
 
-    logger.log(&record, YELLOW)
+	logger.log(&record, YELLOW)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Warning(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, WARNING, args...)
+	record := Record{}
+	record.Build(message, 1, WARNING, args...)
 
-    logger.log(&record, YELLOW)
+	logger.log(&record, YELLOW)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Info(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, INFO, args...)
+	record := Record{}
+	record.Build(message, 1, INFO, args...)
 
-    logger.log(&record, RESET)
+	logger.log(&record, RESET)
 
-    return &record
+	return &record
 }
 
 func (logger *Logger) Debug(message string, args ...interface{}) *Record {
-    record := Record{}
-    record.Build(message, 1, DEBUG, args...)
+	record := Record{}
+	record.Build(message, 1, DEBUG, args...)
 
-    logger.log(&record, BLUE)
+	logger.log(&record, BLUE)
 
-    return &record
+	return &record
 }
